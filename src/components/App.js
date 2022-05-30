a041e4fd4714df0bebb8a9c5342e994d5c088a96
Naftali Kulik <nkulik1994@gmail.com>
2022-05-30 14:32:51 -0400

    finished first deliverable

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [listings, updateListings] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+      .then(r => r.json())
+      .then(listings => updateListings(listings))
+  }, [])
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings} />
     </div>
   );
 }
